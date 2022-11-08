67ad53ac0e399522d96b924c2ec06c9bc86c6677
vishnukr2425 <vishnukr2425@gmail.com>
2022-11-08 15:18:10 +0530

    vishnu commit

--- expressweb/src/app.js
+++ expressweb/src/app.js
@@ -1,17 +1,26 @@
 const express= require("express");
 const app = express();
+const path= require("path");
+const hbs=require("hbs");
 const port= process.env.PORT || 3000;
 
+const staticpath= path.join(__dirname, "../public");
+
+app.use(express.static(staticpath));
+app.set("view engine", "hbs");
+
 app.get("/", (req, res)=>{
-    res.send("Hello from othet side");
+    res.render("index");
 })
 app.get("/about", (req, res)=>{
-    res.send("Hello from about page");
+    res.render("about");
+})
+app.get("/weather", (req, res)=>{
+    res.render("weather");
 })
 app.get("/*", (req, res)=>{
-    res.send("Error 404");
+    res.render("errorpg");
 })
-
 app.listen(port, ()=>{
     console.log(`listening from port number ${port}`);
 })
